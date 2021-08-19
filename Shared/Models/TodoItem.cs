37e4dfa3cd596c371f1dc6ed21bb69111e43e99c
Alex Remo <85962119+aremo-ms@users.noreply.github.com>
2021-08-18 22:56:01 -0400

    solution cleanup + one small bug (#45)
    * solution cleanup + one small bug
    
    * reverted back configuration change because it hardcoded inside PS scripts
    
    * updated to latest identity packages

--- Shared/Models/TodoItem.cs
+++ Shared/Models/TodoItem.cs
@@ -1,7 +1,7 @@
 ﻿/*
  The MIT License (MIT)
 
-Copyright (c) 2018 Microsoft Corporation
+Copyright (c) 2015 Microsoft Corporation
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,9 +20,9 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
- */
+*/
 
-namespace TodoListService_ManualJwt.Models
+namespace Shared.Models
 {
     public class TodoItem
     {
